4bee6f630a9e02173ce44f8160e726476d798874
mammarshahid104 <mammarshahid104@gmail.com>
2026-05-29 11:20:50 +0500

    redirect fix 3

--- server.js
+++ server.js
@@ -12,11 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
-
-app.get('/', (req, res) => {
-  res.redirect('/admin.html');
-});
-
 app.use(express.static('public'));
 
 const productRoutes = require('./routes/products');
