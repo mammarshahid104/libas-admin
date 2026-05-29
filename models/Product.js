d8536162d53f6dcaadfc68aa98846d04d3fdc16d
mammarshahid104 <mammarshahid104@gmail.com>
2026-05-29 09:52:34 +0500

    subcategories added

--- models/Product.js
+++ models/Product.js
@@ -1,39 +1,15 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  category: {
-    type: String,
-    required: true,
-    enum: ['mens', 'kids', 'festive']
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  originalPrice: {
-    type: Number
-  },
-  image: {
-    type: String,
-    required: true
-  },
-  label: {
-    type: String,
-    enum: ['New', 'Sale', 'Hot', '']
-  },
-  sizes: [{
-    type: String
-  }],
-  inStock: {
-    type: Boolean,
-    default: true
-  }
-}, {
-  timestamps: true
-});
+  name: { type: String, required: true },
+  category: { type: String, enum: ['mens', 'kids', 'festive'], required: true },
+  subcategory: { type: String, default: '' },
+  price: { type: Number, required: true },
+  originalPrice: { type: Number },
+  image: { type: String },
+  label: { type: String, enum: ['New', 'Sale', 'Hot', ''], default: '' },
+  sizes: [String],
+  inStock: { type: Boolean, default: true }
+}, { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
